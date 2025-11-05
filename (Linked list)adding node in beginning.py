d3590226135cd5efc266e30e69292f2bb3a1eb79
Pranab8822 <pranab7712@gmail.com>
2025-11-05 14:52:46 +0530

    small change

--- (Linked list)adding node in beginning.py
+++ (Linked list)adding node in beginning.py
@@ -9,7 +9,6 @@ class linked_list():
         node2=node(33)
         node3=node(90)
 
-
         node1.next=node2
         node2.next=node3
 
@@ -23,6 +22,21 @@ class linked_list():
         self.head = new_node
 
 
+
+    def add_at_end(self):
+        num = int(input("Enter the value you want to add: "))
+        new_node = node(num)
+    
+    # If the list is empty
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next is not None:
+            current = current.next  # move to next node until last
+        current.next = new_node    # now current is the last node
+
+
     def display(self):
         current = self.head
         while current:
@@ -30,8 +44,13 @@ class linked_list():
             current = current.next
         print("None")
 
-
+choice=int(input("Enter your choice 1.\n Display Linkedlist \n2.Add node at the abeginning \n3.Add node at the end\n:"))
 ll = linked_list()
-ll.display()            # show initial list
-ll.add_at_beginning()   # add new node
-ll.display()    
+if choice==1:
+    ll.display()
+if choice==2:                # show initial list
+    ll.add_at_beginning()
+    ll.display()   
+if choice==3:
+    ll.add_at_end()  
+    ll.display()     
